3a47b4cf013747deb9e9edc35bb41da932b088c1
Axel Nitzschner <axelnitzschner@mac.com>
2024-02-14 11:21:51 +0100

    coords input fields now 0 chars

--- admin/partials/coords-group.php
+++ admin/partials/coords-group.php
@@ -1,7 +1,7 @@
 <div id="<?php echo $args['id']; ?>" class="<?php echo implode(' ', $args['classes']); ?>">
   <input type="<?php echo $args['subtype'][0]; ?>" <?php echo $key != 0 ? 'required' : ''; ?> placeholder="<?php echo $args['placeholder'][0]; ?>" name="<?php echo  $args['name']; ?>[<?php echo $key; ?>][lat]" size="20" value="<?php echo esc_attr($val['lat']); ?>" />
   <input type="<?php echo $args['subtype'][1]; ?>" <?php echo $key != 0 ? 'required' : ''; ?> placeholder="<?php echo $args['placeholder'][1]; ?>" name="<?php echo  $args['name']; ?>[<?php echo $key; ?>][lon]" size="20" value="<?php echo esc_attr($val['lon']); ?>" />
-  <input type="<?php echo $args['subtype'][2]; ?>" placeholder="<?php echo $args['placeholder'][2]; ?>" name="<?php echo  $args['name']; ?>[<?php echo $key; ?>][lab]" size="20" value="<?php echo esc_attr($val['lab']); ?>" />
+  <input type="<?php echo $args['subtype'][2]; ?>" placeholder="<?php echo $args['placeholder'][2]; ?>" name="<?php echo  $args['name']; ?>[<?php echo $key; ?>][lab]" size="40" value="<?php echo esc_attr($val['lab']); ?>" />
 
   <a href="." class="button button-primary action-remove">
     <i class="dashicons dashicons-trash" style="font-size: 1.3em; vertical-align: middle"></i>
